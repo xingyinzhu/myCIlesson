6a94abda21fcab4f850307ad95cb090314ab5713
XingyinZhu <xingyinzhu@gmail.com>
2013-03-24 21:46:04 +0800

    4th lesson …

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:8888/CI/index.php/';
+$config['base_url']	= 'http://localhost:8888/CI/';
 
 $config['server_root'] = $_SERVER['DOCUMENT_ROOT'];
 
--- application/controllers/email.php
+++ application/controllers/email.php
@@ -12,26 +12,54 @@
 		}
 
 		function index(){
-
-		$this->load->library('email');
-		$this->email->set_newline("\r\n");
-		$this->email->from('xingyinzhu@gmail.com','xyzhu');
-		$this->email->to('xingyinzhu@163.com');
-		$this->email->subject('This is an email test');
-		$this->email->message('It is working. Great');
-
-		$path = $this->config->item('server_root');
-		$file = $path . '/CI/license.txt';
-
-		$this->email->attach($file);
-
-		if($this->email->send()){
-			echo "Your email was sent. fool.";
-		}else{
-			show_error($this->email->print_debugger());
-		}
+			$this->load->view('newsletter');
 
 		}
 
+		function send(){
+
+		$this->load->library('form_validation');
+
+		$this->form_validation->set_rules('name', 'Name', 'trim|required');
+		$this->form_validation->set_rules('email', 'Email Address', 'trim|required|valid_email');
+
+		if($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('newsletter');
+		}
+		else
+		{
+			//passed
+			$name = $this->input->post('name');
+			$email = $this->input->post('email');
+
+			//echo "hello from send"; die();
+
+			$this->load->library('email');
+			$this->email->set_newline("\r\n");
+			$this->email->from('xingyinzhu@gmail.com','xyzhu');
+			$this->email->to($email);
+			$this->email->subject('This is an email test');
+			$this->email->message('It is working. Great');
+
+			$path = $this->config->item('server_root');
+			$file = $path . '/CI/license.txt';
+
+			$this->email->attach($file);
+
+			if($this->email->send()){
+				//echo "Your email was sent. fool.";
+				$this->load->view('signup_confirmation_view');
+			}else{
+				show_error($this->email->print_debugger());
+			}
+
+		}
+
+
+		}
+		
+
+		
 	}	
 ?>
