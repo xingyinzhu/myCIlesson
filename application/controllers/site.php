9b8a1af44fdebe8b6a508fb4f5c123299d753c40
XingyinZhu <xingyinzhu@gmail.com>
2013-03-25 20:01:31 +0800

    7th lesson …

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -21,21 +21,66 @@ class Site extends CI_Controller {
 		$this->load->view('about');
 	}
 	*/
-	function index(){
+
+
+	//function index(){
 		/*
 		$this->load->model('data_model');
 		$data['rows'] = $this->data_model->getAll();
 
 		$this->load->view('home', $data);
 		*/
-		$data = array();
+		//$data = array();
 
-		if ($query = $this->site_model->get_records())
-		{
-			$data['records'] = $query;
-		}
+		//if ($query = $this->site_model->get_records())
+		//{
+			//$data['records'] = $query;
+		//}
 
-		$this->load->view('options_view' , $data);
+		//$this->load->view('options_view' , $data);
+	//}
+
+	function index(){
+
+
+		$this->load->library('pagination');
+		$this->load->library('table');
+
+		$this->table->set_heading('Id','The Title','The Author','The Content');
+
+		$config['base_url'] = 'http://localhost:8888/CI/index.php/site/index';
+		$config['total_rows'] = $this->db->get('data')->num_rows();
+		$config['per_page'] = 10;
+		$config['uri_segment'] = 3;
+		$config['full_tag_open'] = '<div id="pagination">';
+		$config['full_tag_close'] = '</div>';
+		/*
+		$config['num_links'] = 20;
+		$config['full_tag_open'] = '<p>';
+		$config['full_tag_close'] = '</p>';
+		$config['first_link'] = 'First';
+		$config['first_tag_open'] = '<div>';
+		$config['first_tag_close'] = '</div>';
+		$config['last_link'] = 'Last';
+		$config['last_tag_open'] = '<div>';
+		$config['last_tag_close'] = '</div>';
+		$config['next_link'] = '&gt;';
+		$config['next_tag_open'] = '<div>';
+		$config['next_tag_close'] = '</div>';
+		$config['prev_link'] = '&lt;';
+		$config['prev_tag_open'] = '<div>';
+		$config['prev_tag_close'] = '</div>';
+		$config['cur_tag_open'] = '<b>';
+		$config['cur_tag_close'] = '</b>';
+		*/
+
+		$this->pagination->initialize($config);
+
+		$data['records'] = $this->db->get('data', $config['per_page'] , $this->uri->segment(3));
+		
+		$this->load->view('site_view', $data);
+		//echo $this->pagination->create_links();
+
 	}
 
 	
@@ -73,12 +118,14 @@ class Site extends CI_Controller {
 		$this->load->view('members_area');
 	}
 
+	/*
 	function __construct()
 	{
 		parent::__construct();
 		$this->is_logged_in();
 
 	}
+	*/
 
 	function is_logged_in()
 	{
@@ -94,4 +141,3 @@ class Site extends CI_Controller {
 }
 
 ?>
-
---
+++ application/views/site_view.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/style1.css">
+</head>
+<body>
+	<div id="container">
+		<h1>Super Pagination with CodeIgniter</h1>
+		<?php echo $this->table->generate($records); ?>
+
+		<?php echo $this->pagination->create_links(); ?>
+	</div>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+
+<script type="text/javascript" charset="utf-8">
+	$('tr:add').css('background','#aaaaa');
+</script>
+
+</body>
+
+</html>
